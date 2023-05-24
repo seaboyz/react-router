c37fe0e62ea1d16ce4f2802813dc77f323440621
seaboyz <seaboyz@hotmail.com>
2023-05-24 14:33:50 -0500

    Add default route for no match route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About.js";
 import Home from "./components/Home.js";
 import NavBar from "./components/NavBar.js";
 import Order from "./components/Order.js";
+import NoMatch from "./components/NoMatch.js";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="order" element={<Order />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
