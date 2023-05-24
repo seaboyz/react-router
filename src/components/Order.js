56ad126d5f0916cdd3d28630469be799fa3be1a8
seaboyz <seaboyz@hotmail.com>
2023-05-24 14:20:25 -0500

    Add back button to navigate back using useNavigate hook

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Order = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      Order Confirmed
+      <span>Order Confirmed</span>
+      <button onClick={() => navigate(-1)} style={{ margin: "1rem" }}>Back</button>
     </div>
   );
 };
