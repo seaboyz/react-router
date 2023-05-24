99b0890247a44f66a728df39934f5fe126a947e2
seaboyz <seaboyz@hotmail.com>
2023-05-24 13:14:48 -0500

    Connect "/" to <Home/> and "about" to <About/>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About.js";
+import Home from "./components/Home.js";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
   );
 }
 
