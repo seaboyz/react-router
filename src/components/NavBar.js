22f35de9f1bb7940014827cdb85a390ff0c960b7
seaboyz <seaboyz@hotmail.com>
2023-05-24 13:43:20 -0500

    Change Link to NavLink to use the isActive parameter to style links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline"
+    };
+  };
+
   return (
-    <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
-      <Link to="/">Home</Link>
-      <Link to="about">About</Link>
+    <nav style={{ display: "flex", gap: "1rem", padding: "1rem", backgroundColor: "grey" }}>
+      <NavLink
+        style={navLinkStyles}
+        to="/"
+      >Home
+      </NavLink>
+      <NavLink
+        style={navLinkStyles}
+        to="/about"
+      >About
+      </NavLink>
     </nav>
   );
 };
